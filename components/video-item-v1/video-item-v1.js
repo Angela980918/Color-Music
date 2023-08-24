afa9f4c343f017c152effd754d8e4857dee205b4
袁澧林 <kobe980918@gmail.com>
2023-08-24 17:26:39 +0800

    封装video-item组件

---
+++ components/video-item-v1/video-item-v1.js
@@ -0,0 +1,27 @@
+// components/video-item-v1/video-item-v1.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 定义props参数
+    itemData:{
+      type:Object,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
