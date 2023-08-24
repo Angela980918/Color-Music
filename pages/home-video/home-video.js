63c8279ea8dc45a467188f0eadb6dde48ce9e55d
袁澧林 <kobe980918@gmail.com>
2023-08-24 17:26:54 +0800

    视频首页

--- pages/home-video/home-video.js
+++ pages/home-video/home-video.js
@@ -1,66 +1,63 @@
 // pages/home-video/home-video.js
-Page({
+// 请求接口
+import { getTopMV } from "../../service/api_video";
+// import { MusicRequest } from "../../service/index";
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    videoList: [],
+  },
 
+  // 封装MV网络请求函数
+  async fetchTopMVData() {
+    const res = await getTopMV()
+    this.setData({
+      videoList: res.data
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad() {
+    this.fetchTopMVData();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
