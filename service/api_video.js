3f984b98bc6fc0554ba4b573a68d11a05adab3e8
袁澧林 <kobe980918@gmail.com>
2023-08-28 17:23:31 +0800

    更新 api_video.js

--- service/api_video.js
+++ service/api_video.js
@@ -1,13 +1,58 @@
 // 二次封装 video相关的网络请求
 import { MusicRequest } from "./index";
 
-// 获取MV TOP榜单 limit->取出数量 offset->偏移量
+/**
+ *  获取MV TOP榜单 limit->取出数量 offset->偏移量
+ * @param {*} limit
+ * @param {*} offset
+ */
 export function getTopMV(limit = 10, offset = 0) {
   return MusicRequest.get({
     url: "/top/mv",
     data: {
       limit,
       offset,
+    },
+  });
+}
+
+/**
+ * 获取MV 播放地址
+ * @param mvid
+ */
+export function getMVUrl(mvid) {
+  return MusicRequest.get({
+    url: "/mv/url",
+    data: {
+      id: mvid,
+    },
+  });
+}
+
+/**
+ * 获取MV详情
+ * @param mvid
+ * @returns
+ */
+export function getMVInfo(mvid) {
+  return MusicRequest.get({
+    url: "/mv/detail",
+    data: {
+      mvid,
+    },
+  });
+}
+
+/**
+ * 
+ * @param {*} mvid 
+ * @returns 
+ */
+export function getMVRelated(mvid) {
+  return MusicRequest.get({
+    url: "/related/allvideo",
+    data: {
+      id: mvid,
     },
   });
 }
