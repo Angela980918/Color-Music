ba9d223ffd3e9f7085e8cf5eb8e1e32481ebca4e
袁澧林 <kobe980918@gmail.com>
2023-08-29 17:30:16 +0800

    更新 home-music.js

--- pages/home-music/home-music.js
+++ pages/home-music/home-music.js
@@ -1,66 +1,70 @@
 // pages/home-music/home-music.js
+import { getMusicBanner } from "../../service/api_music";
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    searchValue: "", // 查询关键字
+    bannerImg: [], // 轮播图
+  },
 
+  // 搜索框事件监听
+  onSearchClick() {
+    console.log("点击了搜索框");
+    wx.navigateTo({
+      url: "/pages/search-detail/search-detail",
+    });
+  },
+
+  // 获取banner数据
+  async fetchBannerData() {
+    const res = await getMusicBanner();
+    // console.log("banner:", "res");
+    this.setData({
+      bannerImg: res.banners,
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.fetchBannerData();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
