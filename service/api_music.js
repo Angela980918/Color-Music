b41b03e960afa9c05d07554eb9da2d42679158d8
袁澧林 <kobe980918@gmail.com>
2023-08-31 17:34:15 +0800

    更新 api_music.js

--- service/api_music.js
+++ service/api_music.js
@@ -3,14 +3,27 @@ import { MusicRequest } from "./index";
 
 /**
  * 获取banner( 轮播图 ) 数据
- * @param {*} type 
- * @returns 
+ * @param {*} type
+ * @returns
  */
 export function getMusicBanner(type = 1) {
   return MusicRequest.get({
     url: "/banner",
     data: {
-      type
+      type,
+    },
+  });
+}
+
+/**
+ * 获取歌单详情
+ * @param {*} id 歌单ID
+ */
+export function getMusicMenuDetail(id) {
+  return MusicRequest.get({
+    url: "/playlist/detail",
+    data: {
+      id,
     },
   });
 }
