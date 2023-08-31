7f88ac1a9609a70b7ce3a9267f310daffab4937a
袁澧林 <kobe980918@gmail.com>
2023-08-31 17:34:02 +0800

    更新音乐头部组件

--- components/area-header/area-header.js
+++ components/area-header/area-header.js
@@ -4,20 +4,33 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    title: {
+      type: String,
+      value: "默认标题",
+    },
+    showRight: {
+      type: Boolean,
+      value: true,
+    },
+    rightText:{
+      type: String,
+      value: "更多",
+    }
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
-})
+    onRightClick(){
+      // 发送事件给父组件
+      // console.log("rightclick");
+      this.triggerEvent("rightclick");
+    }
+  },
+});
