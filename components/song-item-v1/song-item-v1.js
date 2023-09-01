e3885972846cd2715c43da5104560acb7c1bd788
袁澧林 <kobe980918@gmail.com>
2023-09-01 11:54:12 +0800

    新建歌曲item组件
    v1

---
+++ components/song-item-v1/song-item-v1.js
@@ -0,0 +1,26 @@
+// components/song-item-v1/song-item-v1.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    songItem: {
+      type: Object,
+      value: {},
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
