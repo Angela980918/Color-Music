de677ba1f2617d6fff45866e79279c06220437cc
袁澧林 <kobe980918@gmail.com>
2023-09-04 17:32:47 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 // app.js
 App({
-})
+  /**
+   * 全局设备信息(设备宽度、高度等)
+   */
+  globalData: {
+    screenWidth: 0, // 屏幕宽度
+    screenHeight: 0, // 屏幕高度
+
+  },
+
+  onLaunch() {
+    // 获取设备信息
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.screenWidth = res.screenWidth
+        this.globalData.screenHeight = res.screenHeight
+      }
+    })
+  },
+});
