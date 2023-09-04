10e8e8a0749f99e80e48f74b7b2e016beb125f39
袁澧林 <kobe980918@gmail.com>
2023-09-04 17:32:45 +0800

    更新歌曲列表组件

---
+++ components/song-menu-area/song-menu-area.js
@@ -0,0 +1,36 @@
+// components/song-menu-area/song-menu-area.js
+// 实例化app对象 获取app.js中公共数据
+const app = getApp();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    menuTitle: {
+      type: String,
+      value: "默认歌单标题",
+    },
+    songMenuList: {
+      type: Array,
+      value: [],
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    screenWidth: app.globalData.screenWidth, // 屏幕宽度
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 点击更多
+    OnMenuMoreClick(){
+      console.log("OnMenuMoreClick");
+    }
+
+  },
+});
