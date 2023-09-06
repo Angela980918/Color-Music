c499683eb56f6e91041eeaa6075dad3035eeff98
袁澧林 <kobe980918@gmail.com>
2023-09-06 17:27:44 +0800

    更新歌单item组件

--- components/rank-item/rank-item.js
+++ components/rank-item/rank-item.js
@@ -7,20 +7,32 @@ Component({
     rankItem: {
       type: Object,
       value: {},
-    }
+    },
+    key: {
+      // 榜单类型,默认为新歌榜
+      type: String,
+      value: "newRanking",
+    },
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
-})
+    /**
+     * 歌单详情页跳转
+     *  */
+    onRankItemClick() {
+      const key = this.properties.key;
+      // console.log("onRankItemClick", key);
+      wx.navigateTo({
+        url: `/pages/songs-detail/songs-detail?type=ranking&key=${key}`,
+      });
+    },
+  },
+});
