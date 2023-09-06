bd356f87761cbb2482a8cace97656ad036e2228b
袁澧林 <kobe980918@gmail.com>
2023-09-06 17:28:45 +0800

    更新歌单item

--- components/song-menu-item/song-menu-item.js
+++ components/song-menu-item/song-menu-item.js
@@ -8,20 +8,28 @@ Component({
     menuItem: {
       type: Object,
       value: {},
-    }
+    },
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
-})
+    /**
+     * 点击歌单跳转至歌单详情页
+     */
+    onMenuItemClick() {
+      // console.log("点击歌单跳转");
+      // console.log(this.properties.menuItem.id);
+      const id = this.properties.menuItem.id;
+      wx.navigateTo({
+        url: `/pages/songs-detail/songs-detail?type=menu&id=${id}`,
+      });
+    },
+  },
+});
