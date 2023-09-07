677275950685f592f698e77f59b1936c60de5416
袁澧林 <kobe980918@gmail.com>
2023-09-07 17:31:54 +0800

    创建 api_player.js

---
+++ service/api_player.js
@@ -0,0 +1,17 @@
+// 二次封装 player相关的网络请求
+import { MusicRequest } from "./index";
+
+/**
+ * 根据歌曲id获取歌曲详情
+ * 可以一次性请求多个歌曲详情(用","隔开)
+ * @param {*} ids 歌曲id
+ * @returns
+ */
+export function getSongDetail(ids) {
+  return MusicRequest.get({
+    url: "/song/detail",
+    data: {
+      ids,
+    },
+  });
+}
