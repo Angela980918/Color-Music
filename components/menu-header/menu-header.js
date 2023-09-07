d30d6f89eed3e430dd42c6c735a6fa36f7842221
袁澧林 <kobe980918@gmail.com>
2023-09-07 17:30:26 +0800

    新建音乐列表头部组件
    展示图片、创建者等信息

---
+++ components/menu-header/menu-header.js
@@ -0,0 +1,22 @@
+// components/menu-header/menu-header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    songItem: {
+      type: Object,
+      value: {},
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
