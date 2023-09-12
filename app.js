4e0a760fce577caccd1d2eedb57e05a0c01b323a
袁澧林 <kobe980918@gmail.com>
2023-09-12 17:25:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ App({
     screenWidth: 0, // 屏幕宽度
     screenHeight: 0, // 屏幕高度
     statusBarHeight: 0, // 状态栏高度
+    contentHeight: 0, // 内容高度高度
   },
 
   onLaunch() {
     // 获取设备信息
     wx.getSystemInfo({
       success: (res) => {
-        // console.log(res)
+        console.log(res);
         this.globalData.statusBarHeight = res.statusBarHeight;
         this.globalData.screenWidth = res.screenWidth;
         this.globalData.screenHeight = res.screenHeight;
+        this.globalData.contentHeight =
+          res.screenHeight - res.statusBarHeight - 44;
+        // console.log(
+        //   res.screenHeight - res.statusBarHeight,
+        //   res.screenHeight - res.statusBarHeight - 44
+        // );
       },
     });
   },
