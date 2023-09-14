ef9b81d9b0bda6318316794dec443d78d4259806
袁澧林 <kobe980918@gmail.com>
2023-09-14 17:29:28 +0800

    更新音乐播放页面

--- pages/music-player/music-player.js
+++ pages/music-player/music-player.js
@@ -11,6 +11,9 @@ Page({
     id: 0, // 歌曲id
     currentSong: {}, // 当前播放歌曲
     songLyric: {}, //歌词
+    currentTime: 0, // 当前播放时间
+    durationTime: 0, // 歌曲的总时长
+    sliderValue: 0, // 播放进度(滑块进度条)
 
     statusHeight: 20, // 状态栏高度
     contentHeight: 0, // 内容区域高度
@@ -25,6 +28,7 @@ Page({
     console.log("fetchSongDetail", res);
     this.setData({
       currentSong: res.songs[0],
+      durationTime: res.songs[0].dt,
     });
   },
 
@@ -53,6 +57,27 @@ Page({
     this.setData({ currentPage: index });
   },
 
+  // 点击滑块
+  onSliderChange(event) {
+    console.log("onSliderChange", event);
+
+    const value = event.detail.value;
+    const currentTime = value / 100 * this.data.durationTime;
+
+    audioContext.seek(currentTime / 1000)
+
+    // 设置滑块进度
+    this.setData({
+      currentTime: currentTime,
+      sliderValue: value,
+    });
+  },
+
+  // 滑动滑块
+  onSliderChanging(event) {
+    console.log("onSliderChanging", event);
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -71,7 +96,15 @@ Page({
 
     // 播放进度
     audioContext.onTimeUpdate(() => {
-      // console.log("onTimeUpdate", audioContext.currentTime);
+      this.setData({
+        // 秒数==> 毫秒
+        currentTime: audioContext.currentTime * 1000,
+      });
+
+      // 滑块进度
+      const sliderValue =
+        (this.data.currentTime / this.data.durationTime) * 100;
+      this.setData({ sliderValue });
     });
 
     // 设备信息
