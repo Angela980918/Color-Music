de146660ebd8307ad30c681e1adb3023a7b92889
袁澧林 <kobe980918@gmail.com>
2023-09-28 15:52:55 +0800

    Update playerStore.js

--- store/playerStore.js
+++ store/playerStore.js
@@ -1,5 +1,10 @@
 import { HYEventStore } from "hy-event-store";
 
-const playerStore = new HYEventStore({});
+const playerStore = new HYEventStore({
+  state: {
+    playSongList: [], //当前播放列表
+    playSongIndex: 0, //当前播放歌曲索引
+  },
+});
 
 export default playerStore;
